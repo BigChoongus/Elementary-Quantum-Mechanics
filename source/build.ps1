f47edd3377228abeecb60538e6f3f0507d9ae080
Han-Sen Choong <han-sen.choong23@imperial.ac.uk>
2025-02-25 12:48:46 +0000

    Add automated build

---
+++ source/build.ps1
@@ -0,0 +1,33 @@
+for ($i=1; $i -le 9; $i++) {
+    $prev = $i - 1
+    $next = $i + 1
+
+    # Generate navigation links dynamically
+    if ($prev -ge 1) {
+        $prev_link = "<a href='$prev.html'>&larr; Previous Chapter</a>"
+    } else {
+        $prev_link = ""
+    }
+    
+    if ($next -le 9) {
+        $next_link = "<a href='$next.html'>Next Chapter &rarr;</a>"
+    } else {
+        $next_link = ""
+    }
+
+    # Replace placeholders in footer.html and save a temp version
+    (Get-Content "footer.html") -replace "{{prev_link}}", $prev_link -replace "{{next_link}}", $next_link | Set-Content "footer-temp.html"
+
+    Write-Output "Converting $i.tex to docs/$i.html..."
+    pandoc --mathjax -s "$i.tex" `
+        -include-in-header "pandoc-macros.tex" `  # Fix: Single dash (`-` instead of `--`)
+        -include-in-header "mathjax-config.html" `
+        -include-before-body "preamble.tex" `
+        -include-after-body "footer-temp.html" `
+        -c "style.css" `
+        -o "../docs/$i.html"
+}
+
+# Cleanup temp footer file
+Remove-Item "footer-temp.html"
+
